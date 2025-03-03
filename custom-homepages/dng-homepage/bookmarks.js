27dc0b8a31924509ff7c6cd31f652483fa336b25
Zawar Gondal <53140375+ZawarG@users.noreply.github.com>
2025-03-03 14:26:23 -0500

    Add files via upload

---
+++ custom-homepages/dng-homepage/bookmarks.js
@@ -0,0 +1,28 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "NBA Streams", url: "https://reddit.rnbastreams.com/" },
+      { name: "Torrents", url: "https://thepiratebay.org/index.html" }
+    ],  
+  },
+  {
+    title: "School",
+    links: [
+      { name: "Gmail", url: "https://mail.google.com/mail/u/0/" },
+      { name: "Classroom", url: "https://classroom.google.com/u/0" },
+      { name: "Pastpapers", url: "https://papers.gceguide.com/" },
+    ],
+
+  },
+
+  {
+    title: "Social",
+    links: [
+      { name: "Instagram", url: "https://twitter.com" },
+      { name: "Whatsapp", url:"https://web.whatsapp.com" },
+    ],
+  },
+];
